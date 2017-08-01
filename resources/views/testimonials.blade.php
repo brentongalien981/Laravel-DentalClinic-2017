2c0f42ccce4ca84377f89a2b113fa58cb8b26028
odoxtee person <odox700@gmail.com>
2017-08-01 06:49:38 -0400

    - Added testimonials

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -1,15 +1,158 @@
-<div class="container-fluid text-left" id="testimonials-main-container">
-    <h3 id="testimonials-title">Testimonials</h3>
+<div class="container-fluid" id="testimonials-main-container">
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+            <h3 id="testimonials-title">Testimonials</h3>
+        </div>
+    </div>
+
+    <div class="section" id="carousel">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+
+                    <!-- Carousel Card -->
+                    <div class="card card-raised card-carousel">
+                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                            <div class="carousel slide" data-ride="carousel">
+
+                                <!-- Indicators -->
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                                </ol>
+
+                                <!-- Wrapper for slides -->
+                                <div class="carousel-inner">
+
+                                    <div class="item active">
+                                        <img src="img/dental-selfie.jpg" alt="Awesome Image">
+
+                                        <div class="carousel-caption testimonial-text-container">
+                                            <h4 class="testimonial-text">It's all about the doctor..</h4>
+
+                                            <p>
+                                                <q class="testimonial-text">
+                                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                                                    ea
+                                                    commodo consequat.
+                                                </q>
+                                            </p>
+
+                                            <h5 class="testimonial-text"><i class="fa fa-star-o"></i> 9 / 10</h5>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="img/clinic4.jpg" alt="Awesome Image">
+
+                                        <div class="carousel-caption testimonial-text-container">
+                                            <h4 class="testimonial-text">Awesome service!</h4>
+
+                                            <p>
+                                                <q class="testimonial-text">
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                                    eiusmod
+                                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                                                    ea
+                                                    commodo consequat.
+                                                </q>
+                                            </p>
+
+                                            <h5 class="testimonial-text"><i class="fa fa-star-o"></i> 10 / 10</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="img/team2.jpg" alt="Awesome Image">
+                                        <div class="carousel-caption testimonial-text-container">
+                                            <h4 class="testimonial-text">The staff is super accomodating</h4>
+
+                                            <p>
+                                                <q class="testimonial-text">
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                                    eiusmod
+                                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                                    minim...
+                                                </q>
+                                            </p>
+
+                                            <h5 class="testimonial-text"><i class="fa fa-star-o"></i> 9.5 / 10</h5>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="item">
+                                        <img src="img/dental-checkup.jpg" alt="Awesome Image">
+
+                                        <div class="carousel-caption testimonial-text-container">
+                                            <h4 class="testimonial-text">I was so confident..</h4>
+
+                                            <p>
+                                                <q class="testimonial-text">
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                                    eiusmod
+                                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                                                    ea
+                                                    commodo consequat.
+                                                </q>
+                                            </p>
+
+                                            <h5 class="testimonial-text"><i class="fa fa-star-o"></i> 10 / 10</h5>
+                                        </div>
+
+                                    </div>
+
+                                    <!-- Controls -->
+                                    <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                        <i class="material-icons">keyboard_arrow_left</i>
+                                    </a>
+                                    <a class="right carousel-control" href="#carousel-example-generic"
+                                       data-slide="next">
+                                        <i class="material-icons">keyboard_arrow_right</i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Carousel Card -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </div>
-
-
-
 
 
 <style>
     #testimonials-main-container {
-        /*background-color: rgb(249, 249, 249);*/
+        background-color: rgb(249, 249, 249);
         padding: 80px 40px;
         background-color: white;
+    }
+
+    p q {
+        font-style: italic;
+    }
+
+    .testimonial-text-container {
+        background-color: rgba(120, 120, 120, 0.1);
+        padding: 5px 20px;
+        border-radius: 20px;
+        /*box-shadow: ;*/
+        /*padding-bottom: -30px;*/
+    }
+
+    .testimonial-text {
+
+        text-shadow: 1px 1px 1px rgb(1, 1, 1);
     }
 </style>
